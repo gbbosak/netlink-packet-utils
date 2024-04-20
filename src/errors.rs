b92483a8bb289ed49ab30a41f02ab671c3fdf3ad
Miguel Flores Ruiz de Eguino <1889916+miguelfrde@users.noreply.github.com>
2024-04-19 17:57:57 -0700

    WIP: typed errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 
+use crate::nla;
 use anyhow::anyhow;
 use thiserror::Error;
 
@@ -32,29 +33,63 @@ impl From<anyhow::Error> for EncodeError {
 }
 
 #[derive(Debug, Error)]
-#[error("Decode error occurred: {inner}")]
-pub struct DecodeError {
-    inner: anyhow::Error,
+pub enum DecodeError {
+    #[error("Invalid MAC address")]
+    InvalidMACAddress,
+
+    #[error("Invalid IPv6 address")]
+    InvalidIPv6Address,
+
+    #[error("Invalid string")]
+    Utf8Error(#[from] std::string::FromUtf8Error),
+
+    #[error("Invalid u8")]
+    InvalidU8,
+
+    #[error("Invalid u16")]
+    InvalidU16,
+
+    #[error("Invalid u32")]
+    InvalidU32,
+
+    #[error("Invalid u64")]
+    InvalidU64,
+
+    #[error("Invalid u128")]
+    InvalidU128,
+
+    #[error("Invalid i32")]
+    InvalidI32,
+
+    #[error(transparent)]
+    InvalidNLABuffer(#[from] NLAError),
+
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
 }
 
 impl From<&'static str> for DecodeError {
     fn from(msg: &'static str) -> Self {
-        DecodeError {
-            inner: anyhow!(msg),
-        }
+        DecodeError::Other(anyhow!(msg))
     }
 }
 
 impl From<String> for DecodeError {
     fn from(msg: String) -> Self {
-        DecodeError {
-            inner: anyhow!(msg),
-        }
+        DecodeError::Other(anyhow!(msg))
     }
 }
 
-impl From<anyhow::Error> for DecodeError {
-    fn from(inner: anyhow::Error) -> DecodeError {
-        DecodeError { inner }
-    }
+#[derive(Debug, Error)]
+pub enum NLAError {
+    #[error("buffer has length {buffer_len}, but an NLA header is {} bytes", nla::TYPE.end)]
+    BufferTooSmall { buffer_len: usize },
+
+    #[error("buffer has length: {buffer_len}, but the NLA is {nla_len} bytes")]
+    LengthMismatch { buffer_len: usize, nla_len: u16 },
+
+    #[error(
+        "NLA has invalid length: {nla_len} (should be at least {} bytes", nla::TYPE.end
+    )]
+    InvalidLength { nla_len: u16 },
 }
